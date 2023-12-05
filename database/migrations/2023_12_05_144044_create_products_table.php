a7681295397ce714975f52c2a71a37e917c77cb9
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2023-12-05 21:01:34 +0600

    products table columns added

--- database/migrations/2023_12_05_144044_create_products_table.php
+++ database/migrations/2023_12_05_144044_create_products_table.php
@@ -15,7 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name',50);
+            $table->double('price');
+            $table->string('description',100);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
